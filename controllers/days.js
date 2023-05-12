052c1f2cd744af6c1777b88affb1ab255be030c8
Peter Lucido <petermlucido@gmail.com>
2023-05-12 14:58:36 -0500

    create the create function

--- controllers/days.js
+++ controllers/days.js
@@ -13,6 +13,24 @@ async function index(req, res) {
   }
 }
 
+async function create(req, res) {
+  try {
+    req.body.ower = req.user.profile
+    const day = await Day.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { days: day } },
+      { new: true }
+    )
+    day.owner = profile
+    res.status(201).json(blog)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
 export {
   index,
+  create,
 }
